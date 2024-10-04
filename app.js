7ad2384418f18d862fbd70863b22eb35542a0a87
Estelito Y. Buenavista II <estelitobuenavista@gmail.com>
2024-10-04 14:18:41 +0800

    added rate Limiter

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import bodyParser from "body-parser"
 import userRoute from "./routes/user.js"
+import rateLimiter from "./middleware/rateLimiter.js";
+import loggerMiddleware from "./middleware/detailLogger.js";
 
 const app = express();
 
@@ -8,6 +10,8 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use('/', userRoute);
+app.use(rateLimiter());
+app.use(loggerMiddleware());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
